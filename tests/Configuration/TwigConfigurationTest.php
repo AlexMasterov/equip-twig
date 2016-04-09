183c218afcb1d9d2e1ddcc15e8608dd16cf8b8f5
Alex Masterov <alex.masterow@gmail.com>
2016-04-10 00:37:02 +0300

    Add test

--- tests/Configuration/TwigConfigurationTest.php
+++ tests/Configuration/TwigConfigurationTest.php
@@ -3,6 +3,7 @@
 namespace Asmaster\EquipTwigTests\Configuration;
 
 use Auryn\Injector;
+use Equip\Env;
 use Equip\Configuration\AurynConfiguration;
 use Equip\Responder\FormattedResponder;
 use Asmaster\EquipTwig\TwigFormatter;
@@ -31,5 +32,23 @@ class TwigConfigurationTest extends \PHPUnit_Framework_TestCase
             AurynConfiguration::class,
             TwigConfiguration::class
         ];
+    }
+
+    public function testPrepareFilesystem()
+    {
+        $templatesDir = __DIR__.'/../_templates';
+
+        $injector = new Injector;
+        $injector->prepare(Env::class, function (Env $env) use ($templatesDir) {
+            return $env->withValue('TWIG_TEMPLATES', $templatesDir);
+        });
+
+        $config = $injector->make(TwigConfiguration::class);
+        $config->apply($injector);
+
+        $loader = $injector->make(\Twig_Loader_Filesystem::class);
+
+        $this->assertNotEmpty($loader->getPaths());
+        $this->assertSame($templatesDir, $loader->getPaths()[0]);
     }
 }
