b1bd72174ed95df2d1dd274b3e278c7883b09466
Alex Masterov <alex.masterow@gmail.com>
2016-05-30 02:33:49 +0300

    Add the test for PayloadInterfaceRenderTrait

---
+++ tests/Traits/PayloadInterfaceRenderTraitTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Asmaster\EquipTwig\Tests\Traits;
+
+use Asmaster\EquipTwig\Traits\PayloadInterfaceRenderTrait;
+use Equip\Adr\PayloadInterface;
+use Equip\Payload;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class PayloadInterfaceRenderTraitTest extends TestCase
+{
+    use PayloadInterfaceRenderTrait;
+
+    protected function setUp()
+    {
+        $this->payload = new Payload;
+    }
+
+    public function testPayloadInterface()
+    {
+        $this->assertInstanceOf(PayloadInterface::class, $this->payload);
+    }
+
+    public function testPayloadInterfaceRender()
+    {
+        $template = 'test.html.twig';
+        $output = [
+            'body' => 'body'
+        ];
+
+        $payload = $this->render($template, $output);
+
+        $this->assertEquals($output, $payload->getOutput());
+    }
+}
