af02a4e13d7d29105be24dc8bb1324e92929783e
Alex Masterov <alex.masterow@gmail.com>
2016-05-29 19:28:28 +0300

    Add PayloadInterfaceRenderTrait

---
+++ src/Traits/PayloadInterfaceRenderTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Asmaster\EquipTwig\Traits;
+
+trait PayloadInterfaceRenderTrait
+{
+    /**
+     * @var \Equip\Adr\PayloadInterface
+     */
+    protected $payload;
+
+    /**
+     * @param string $template The template name
+     * @param array  $output   The output produced by the domain layer
+     *
+     * @return PayloadInterface
+     */
+    protected function render($template, array $output = [])
+    {
+        return $this->payload
+            ->withStatus(PayloadInterface::STATUS_OK)
+            ->withSetting('template', $template)
+            ->withOutput($output);
+    }
+}
